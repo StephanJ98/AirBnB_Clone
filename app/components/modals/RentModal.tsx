4a18b897fec762eaf6f027891e7bb4e2b5005814
StephanJ98 <diego.jeandon@gmail.com>
2023-05-16 00:03:16 +0200

    :pencil: Cloudinary Account 1/2

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -10,6 +10,7 @@ import CategoryInput from '../inputs/CategoryInput'
 import CountrySelect from '../inputs/CountrySelect'
 import dynamic from 'next/dynamic'
 import Counter from '../inputs/Counter'
+import ImageUpload from '../inputs/ImageUpload'
 
 enum STEPS {
     CATEGORY = 0,
@@ -141,6 +142,18 @@ export default function RentModal({ }: Props) {
                     value={bathroomCount}
                     onChange={(value) => setCustomValue('bathroomCount', value)}
                 />
+            </div>
+        )
+    }
+
+    if (step == STEPS.IMAGES) {
+        bodyContent = (
+            <div className='flex flex-col gap-8'>
+                <Heading
+                    title='Add a photo of your place'
+                    subtitle='Show guests what your place looks like!'
+                />
+                <ImageUpload />
             </div>
         )
     }
