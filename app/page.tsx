038724927e28af81aeacd340712c6d5015d06213
StephanJ98 <diego.jeandon@gmail.com>
2023-06-12 15:22:41 +0200

    :bug: Fix Error: Dynamic server usage: searchParams.userId

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,8 @@ type Props = {
   searchParams: ListingProps
 }
 
+export const dynamic = 'force-dynamic'
+
 export default async function Home({ searchParams }: Props) {
   const listings = await getListings(searchParams)
   const currentUser = await getCurrentUser()
